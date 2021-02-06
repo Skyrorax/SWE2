c45f85b981a0059157de7e897221a445d603e0c8
BeeIBerry <ericberlin24@gmail.com>
2021-02-06 13:43:55 +0100

    added subpages added language switch added animations to responsive nav-bar

--- index.js
+++ index.js
@@ -3,10 +3,24 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.navitems');
+    const navLinks = document.querySelectorAll('.navitems li')
 
     burger.addEventListener('click', () => {
+        //toggle nav
         console.log('click');
         nav.classList.toggle('nav-active');
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            }
+            else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+        //Burger Animation
+        burger.classList.toggle('toggle');
     });
 }
 
@@ -27,3 +41,4 @@ setInterval(function(){
 }, 5000);
 
 /* sidebar automatic end*/
+
