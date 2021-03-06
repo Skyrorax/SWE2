fd982007bf4cac41c043b96a09a2c43e13d00792
BeeIBerry <ericberlin24@gmail.com>
2021-03-06 19:41:05 +0100

    added submenus for nav-bar added subpages

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-/* navigation-bar transition */
-
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.navitems');
-    const navLinks = document.querySelectorAll('.navitems li')
-
-    burger.addEventListener('click', () => {
-        //toggle nav
-        console.log('click');
-        nav.classList.toggle('nav-active');
-
-        //Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            }
-            else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-            }
-        });
-        //Burger Animation
-        burger.classList.toggle('toggle');
-    });
-}
-
-navSlide();
-
-/* navigation-bar transition end */
-
 /* sidebar automatic */
 
 var counter = 1;
