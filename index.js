96f2b9a9c193205fd40cee92925be5f65814c9b7
BeeIBerry <ericberlin24@gmail.com>
2021-03-09 12:36:38 +0100

    added background image to main page, removed slider & intro text

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-/* sidebar automatic */
 
-var counter = 1;
-
-setInterval(function(){
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
-    if (counter > 5) {
-        counter = 1;
-    }
-}, 5000);
-
-/* sidebar automatic end*/
 
 
